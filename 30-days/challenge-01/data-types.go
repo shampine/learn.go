28c4102ee2c22775372154267f63ade4a39d41f2
patrickshampine <patrick@patrickshampine.com>
2017-12-24 11:31:41 -0800

    uncommitted code?

---
+++ 30-days/challenge-01/data-types.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "bufio"
+)
+
+func main() {
+    var i uint32 = 4
+    var d float32 = 4.0
+    var s string = "HackerRank "
+
+    scanner := bufio.NewReader(os.Stdin)
+
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+
+    var i2 uint32;
+    var d2 float32;
+    var s2 string;
+
+    
