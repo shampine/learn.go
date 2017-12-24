9a70c05167b7a470180de83984c7ca1f408ae1ab
patrickshampine <patrick@patrickshampine.com>
2017-12-24 12:37:37 -0800

    Updated

--- udemy/cards/deck.go
+++ udemy/cards/deck.go
@@ -1,10 +1,12 @@
 package main
 
 import (
-	"os"
-	"fmt"
-	"io/ioutil"
-	"strings"
+    "os"
+    "fmt"
+    "io/ioutil"
+    "strings"
+    "math/rand"
+    "time"
 )
 
 type deck []string
@@ -37,36 +39,48 @@ func newDeck() deck {
 		for _, value := range cardValues {
 			cards = append(cards, value+" of "+suit)
 		}
-	}
+    }
 
-	return cards
+    return cards
 }
 
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
-	}
+    for i, card := range d {
+        fmt.Println(i, card)
+    }
 }
 
 func deal(d deck, handSize int) (deck, deck) {
-	return d[:handSize], d[handSize:]
+    return d[:handSize], d[handSize:]
 }
 
 func (d deck) toString() string {
-	return strings.Join([]string(d), ",")
+    return strings.Join([]string(d), ",")
 }
 
 func (d deck) saveToFile(filename string) error {
-	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+    return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
 
-func newDeckFromFile(filename string) deck, error {
-	bs, err := ioutil.ReadFile(filename)
+func newDeckFromFile(filename string) deck {
+    bs, err := ioutil.ReadFile(filename)
 
-	if (err != nil) {
-		fmt.Println("Error:", err)
-		os.Exit(1)
-	}
+    if (err != nil) {
+        fmt.Println("Error:", err)
+        os.Exit(1)
+    }
 
+    s := strings.Split(string(bs),",")
+    return deck(s)
+}
 
+func (d deck) shuffle() {
+    source := rand.NewSource(time.Now().UnixNano())
+    r := rand.New(source)
+
+    for i := range d {
+        newPosition := r.Intn(len(d) - 1)
+
+        d[i], d[newPosition] = d[newPosition], d[i]
+    }
 }
